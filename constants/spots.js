1dee3ceac5d653a62ac550bb3980d0ee1f7d7212
Rui Silva <rui.silva@diconium.com>
2020-10-22 20:20:56 +0100

    feat: adds Areas.

--- constants/spots.js
+++ constants/spots.js
@@ -1,74 +1,95 @@
-exports.almada = {
-  spots: [
+exports.beaches = {
+  areas: [
     {
-      caption: 'Cabana do Pescador',
-      lat: '47.4113',
-      long: '12.851',
-      map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
-      reports: {
-        url: 'https://beachcam.meo.pt/livecams/costa-de-caparica-cabana-do-pescador/',
-      },
-      stream: 'https://video-auth1.iol.pt/beachcam/bccabanadopescador/playlist.m3u8',
+      caption: 'Almada',
+      spots: [
+        {
+          caption: 'Cabana do Pescador',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/costa-de-caparica-cabana-do-pescador/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/bccabanadopescador/playlist.m3u8',
+        },
+        {
+          caption: 'São João',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-sao-joao/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/costacaparicasaojoao/playlist.m3u8',
+        },
+        {
+          caption: 'Barbas | CDS',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/costa-da-caparica/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/costacaparicacds/playlist.m3u8',
+        },
+        {
+          caption: 'Marcelino | CDS',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-cds/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/costacaparicabernard/playlist.m3u8',
+        },
+        {
+          caption: 'Praia Nova',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-praia-nova/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/bccaparicanovapraia/playlist.m3u8',
+        },
+        {
+          caption: 'Riviera',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-riviera/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/riviera/playlist.m3u8',
+        },
+        {
+          caption: 'Fonte da Telha',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/fonte-da-telha/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/fontedatelha/playlist.m3u8',
+        },
+      ],
     },
     {
-      caption: 'São João',
-      lat: '47.4113',
-      long: '12.851',
-      map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
-      reports: {
-        url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-sao-joao/',
-      },
-      stream: 'https://video-auth1.iol.pt/beachcam/costacaparicasaojoao/playlist.m3u8',
+      caption: 'Cascais',
+      spots: [
+        {
+          caption: 'Carcavelos',
+          lat: '47.4113',
+          long: '12.851',
+          map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
+          reports: {
+            url: 'https://beachcam.meo.pt/livecams/praia-de-carcavelos/',
+          },
+          stream: 'https://video-auth1.iol.pt/beachcam/carcavelos/playlist.m3u8',
+        },
+      ],
     },
-    {
-      caption: 'Barbas | CDS',
-      lat: '47.4113',
-      long: '12.851',
-      map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
-      reports: {
-        url: 'https://beachcam.meo.pt/livecams/costa-da-caparica/',
-      },
-      stream: 'https://video-auth1.iol.pt/beachcam/costacaparicacds/playlist.m3u8',
-    },
-    {
-      caption: 'Marcelino | CDS',
-      lat: '47.4113',
-      long: '12.851',
-      map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
-      reports: {
-        url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-cds/',
-      },
-      stream: 'https://video-auth1.iol.pt/beachcam/costacaparicabernard/playlist.m3u8',
-    },
-    {
-      caption: 'Praia Nova',
-      lat: '47.4113',
-      long: '12.851',
-      map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
-      reports: {
-        url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-praia-nova/',
-      },
-      stream: 'https://video-auth1.iol.pt/beachcam/bccaparicanovapraia/playlist.m3u8',
-    },
-    {
-      caption: 'Riviera',
-      lat: '47.4113',
-      long: '12.851',
-      map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
-      reports: {
-        url: 'https://beachcam.meo.pt/livecams/costa-da-caparica-riviera/',
-      },
-      stream: 'https://video-auth1.iol.pt/beachcam/riviera/playlist.m3u8',
-    },
-    {
-      caption: 'Fonte da Telha',
-      lat: '47.4113',
-      long: '12.851',
-      map: 'https://media.skigebiete-test.de/images/ecu/entity/e_skiresort/ski-resort_schmittenhoehe_n3085-153507-1_raw.jpg',
-      reports: {
-        url: 'https://beachcam.meo.pt/livecams/fonte-da-telha/',
-      },
-      stream: 'https://video-auth1.iol.pt/beachcam/fontedatelha/playlist.m3u8',
-    },
+
   ],
 };
