47dad6b43bcae3abeb785892c3743e13919463a4
Rui Silva <rui.silva@diconium.com>
2020-10-27 22:17:31 +0000

    fix: fix heroku deploy

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ require('dotenv').config();
 const Telegraf = require('telegraf');
 const commands = require('./commands');
 
+const { URL, PORT = '5000' } = process.env;
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 Object.values(commands).map((command) => command(bot));
 
-bot.launch();
+if (URL) {
+  bot.launch({
+    webhook: {
+      domain: URL,
+      port: PORT,
+    },
+  });
+} else {
+  bot.launch();
+}
